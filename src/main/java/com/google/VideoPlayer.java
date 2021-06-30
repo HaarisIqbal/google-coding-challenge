c6b1b49f03ea0c0d4af105acb27eb501c2f3bbd2
Haaris Iqbal <haaris.n.iqbal@gmail.com>
2021-06-30 17:37:17 +0500

    Stop Video Adjust
    Adjusted video stop so that null videoId entry does not stop current video, passes test.

--- src/main/java/com/google/VideoPlayer.java
+++ src/main/java/com/google/VideoPlayer.java
@@ -35,16 +35,17 @@ public class VideoPlayer {
 
   public void playVideo(String videoId) {
 
-    // regardless of whether a valid videoId has been provided to play, current video will stop playing.
-    if (playing) {
-      stopVideo();
-    }
-
     // only proceeding for valid videoId
     if (videoLibrary.getVideo(videoId) != null) {
+
+      if (playing) {
+        stopVideo();
+      }
+
       System.out.println("Playing video: " + videoLibrary.getVideo(videoId).getTitle());
       currentVideoID = videoId; // variable will be updated to new video.
       playing = true;
+
     } else {
       System.out.println("Cannot play video: Video does not exist. Silly User.");
     }
